58a6478c3f80b18ee6d846d4deb22a1cd2bf200a
dogapacal <dogapacal06@gmail.com>
2026-08-21 15:52:41 +0300

    Test dosyalari GitHub'dan kaldirildi

--- test_baglanti.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.llms import Ollama
-
-llm = Ollama(model="tarih-asistani")
-print("Soru gönderiliyor...")
-cevap = llm.invoke("Fatih Sultan Mehmet İstanbul'u ne zaman fethetti?")
-print("Cevap:", cevap)
